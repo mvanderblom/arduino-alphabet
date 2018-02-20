bac36207a53c40956e2b65168acd8070cbbcb212
Martijn van der Blom <m.vanderblom@gmail.com>
2018-02-21 00:20:09 +0100

    working SerialMonitorByteConsumer.cpp with multidimensionalarray

--- SerialMonitorByteConsumer.cpp
+++ SerialMonitorByteConsumer.cpp
@@ -5,17 +5,17 @@
 
 void SerialMonitorByteConsumer::consumeBytes(byte bytes[], int byteArraySize) {
 
-	for (int columnIndex = 0; columnIndex < byteArraySize; columnIndex++) {
+	for (int columnIndex = 1; columnIndex <= byteArraySize; columnIndex++) {
 		byte aByte = bytes[columnIndex];
 		for (int rowIndex = 0; rowIndex < 7; rowIndex++) {
 			int aBit = bitRead(aByte, (7 - rowIndex));
-			Serial.print(aBit== 1 ? "XX" : "oo");
+			Serial.print(aBit == 1 ? F("XX") : F("oo"));
 		}
 		Serial.println();
 	}
 
 	for (int j = 0; j < 7; j++) {
-		Serial.print("oo");
+		Serial.print(F("  "));
 	}
 	Serial.println();
 }
